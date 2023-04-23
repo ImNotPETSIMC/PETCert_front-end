c6e810c6c0da6b32e0815b22ee3938b2b622b6a9
Enzo Weder <enzowederma@gmail.com>
2023-04-23 14:58:44 -0300

    🎉 init: first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './assets/logo.png';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      
+    </div>
+  );
+}
+
+export default App;
