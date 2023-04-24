39dbc62197dbfa89f86a98937d166193888dfdd2
Enzo Weder <enzowederma@gmail.com>
2023-04-24 12:02:56 -0300

    ✨ feat: add api request for certificate

--- src/PETCert.jsx
+++ src/PETCert.jsx
@@ -1,8 +1,26 @@
 import { generateInput } from './Utils';
-import './styles/PETCert.css'
+import './styles/PETCert.css';
+import Axios from "axios";
 
 
 function PETCert() {
+    const getCertificate = () => {
+        Axios.post("http://localhost:8000/getCertificado", {
+            headers: { "Content-Type": "application/json" },
+            body: {
+                "pessoa-certificada": 'Vinicius Pires Barreto',
+                "nome-curso": 'Minicurso de GIT',
+                "tipo-certificado": 'conclusao',
+                "responsaveis-atividade": ['Yan Gustavo Pegyn Silva', 'Roberto Costa Tupinambá'],
+                "cidade-e-data": 'Monte Carmelo MG, 06 de fevereiro de 2023',
+                "nome-assinante": 'Silvio',
+                "cargo-assinatura": 'Tutor'
+            }
+        })
+        .then(response => console.log(response.data))
+    }
+
+    
     const inputNames = ["Nome Curso", "Tipo Certificado", "Pessoa Certificada", "Responsaveis Atividade", "Cidade e Data", "Nome Assinante"];
     
     return (
@@ -10,7 +28,7 @@ function PETCert() {
             <h1>PETCERT</h1>
             <div id='inputs-container'>
                 {inputNames.map((input) => generateInput(input))}
-                <button type="submit" id='getCertificate'>GERAR CERTIFICADO</button>
+                <button type="submit" id='getCertificate' onClick={() => {getCertificate()}}>GERAR CERTIFICADO</button>
             </div>
         </div>
     );
