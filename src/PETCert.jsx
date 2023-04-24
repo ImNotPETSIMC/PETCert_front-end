428b6a46fb1db19b03bb27b74de4fde19bd0a4f0
Enzo Weder <enzowederma@gmail.com>
2023-04-23 22:33:36 -0300

    ✨ feat: create Utils.js

--- src/PETCert.jsx
+++ src/PETCert.jsx
@@ -1,25 +1,6 @@
+import { generateInput } from './Utils';
 import './styles/PETCert.css'
 
-const generateInput = (name, label) => {
-    const aux = String(name).replace(' ', '-');
-    const id = String(aux).toLowerCase();
-    
-    if(!label){
-        return (
-            <div className={id +"-container"}>
-                <input type="text" name="" id={id} />
-                <label htmlFor={id}>{name.toUpperCase()}</label>
-            </div>
-        )
-    };
-    
-    return (
-        <div className={id +"-container"}>
-            <input type="text" name="" id={id} />
-            <label htmlFor={id}>{label}</label>
-        </div>
-    )
-}
 
 function PETCert() {
     const inputNames = ["Nome Curso", "Tipo Certificado", "Pessoa Certificada", "Responsaveis Atividade", "Cidade e Data", "Nome Assinante"];
---
+++ src/Utils.js
@@ -0,0 +1,20 @@
+export const generateInput = (name, label) => {
+    const aux = String(name).replace(' ', '-');
+    const id = String(aux).toLowerCase();
+    
+    if(!label){
+        return (
+            <div className={id +"-container"}>
+                <input type="text" name="" id={id} />
+                <label htmlFor={id}>{name.toUpperCase()}</label>
+            </div>
+        )
+    };
+    
+    return (
+        <div className={id +"-container"}>
+            <input type="text" name="" id={id} />
+            <label htmlFor={id}>{label.toUpperCase()}</label>
+        </div>
+    )
+}
