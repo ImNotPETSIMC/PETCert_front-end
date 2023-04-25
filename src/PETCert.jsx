e18465868aecf23f5b8fce5da6f39cdaaf283e19
Enzo Weder <enzowederma@gmail.com>
2023-04-24 21:09:45 -0300

    🦄 refactor:  move getCertificate into Utils.js

--- src/PETCert.jsx
+++ src/PETCert.jsx
@@ -1,26 +1,8 @@
-import { generateInput } from './Utils';
+import { generateInput, getCertificate } from './Utils';
 import './styles/PETCert.css';
-import Axios from "axios";
 
 
-function PETCert() {
-    const getCertificate = () => {
-        Axios.post("http://localhost:8000/getCertificado", {
-            headers: { "Content-Type": "application/json" },
-            body: {
-                "pessoa-certificada": 'Vinicius Pires Barreto',
-                "nome-curso": 'Minicurso de GIT',
-                "tipo-certificado": 'conclusao',
-                "responsaveis-atividade": ['Yan Gustavo Pegyn Silva', 'Roberto Costa Tupinambá'],
-                "cidade-e-data": 'Monte Carmelo MG, 06 de fevereiro de 2023',
-                "nome-assinante": 'Silvio',
-                "cargo-assinatura": 'Tutor'
-            }
-        })
-        .then(response => console.log(response.data))
-    }
-
-    
+function PETCert() {   
     const inputNames = ["Nome Curso", "Tipo Certificado", "Pessoa Certificada", "Responsaveis Atividade", "Cidade e Data", "Nome Assinante"];
     
     return (
