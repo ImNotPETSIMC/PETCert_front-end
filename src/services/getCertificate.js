0fd1ab47e0a2dc288a9d8ffdfedb4fad20ceda5a
Enzo Weder <enzowederma@gmail.com>
2023-04-26 23:02:32 -0300

    🦄 refactor: reorganize and delete comments in getCertificate
    Remove comments and reorganize PDF creation section of getCertificate

--- src/services/getCertificate.js
+++ src/services/getCertificate.js
@@ -54,12 +54,13 @@ export default async (req) => {
     const veredito = await validarParametros(body);
     if (!veredito["Accept"]) {
       console.log(veredito);
-      return res.status(400).send(veredito["Cause"]).end();
+      //return res.status(400).send(veredito["Cause"]).end();
     }
 
     const tipoCerticado = body["tipo-certificado"];
     if(tipoCerticado == "conclusao") body["tipo-certificado"] = ", concluiu com exito o(a) ";
     else if(tipoCerticado == "participacao") body["tipo-certificado"] = ", participou do(a) ";
+
 
     //Daqui para baixo é onde desenha o PDF
     const doc = new PDFDocument({
@@ -72,20 +73,18 @@ export default async (req) => {
     const grad = doc.linearGradient(0, 0, 0, 100);
     grad.stop(0, "#31FFEF");
     grad.stop(1, "#8CA5FF");
-
-    //doc.pipe(fs.createWriteStream('output.pdf'));
     doc.rect(0, 0, doc.page.width, doc.page.height).fill("#fff");
     doc
-      .fillAndStroke(grad)
-      .lineWidth(20)
-      .lineJoin("round")
-      .rect(
-        distanceMargin,
-        distanceMargin,
-        doc.page.width - distanceMargin * 2,
-        doc.page.height - distanceMargin * 2
-      )
-      .stroke();
+    .fillAndStroke(grad)
+    .lineWidth(20)
+    .lineJoin("round")
+    .rect(
+      distanceMargin,
+      distanceMargin,
+      doc.page.width - distanceMargin * 2,
+      doc.page.height - distanceMargin * 2
+    )
+    .stroke();
 
     const maxWidth = 280;
     const maxHeight = 140;
@@ -99,95 +98,76 @@ export default async (req) => {
       }
     );
 
-    function jumpLine(doc, lines) {
-      for (let index = 0; index < lines; index++) {
-        doc.moveDown();
-      }
-    }
+    const jumpLine = (doc, lines) => { for(let index = 0; index < lines; index++) { doc.moveDown(); } }
 
     jumpLine(doc, 11);
 
     doc
-      .font("Helvetica")
-      .fontSize(28)
-      .fill("#021c27")
-      .font("Times-BoldItalic")
-      .text("Certificado de Conclusão", {
-        //Ou Participação
-        align: "center"
-      });
+    .font("Helvetica")
+    .fontSize(28)
+    .fill("#021c27")
+    .font("Times-BoldItalic")
+    .text("Certificado de Conclusão", { align: "center"});
 
     jumpLine(doc, 1);
 
     doc
-      .fontSize(18)
-      .fill("#021c27")
-      .font("Helvetica")
-      .text("Certificamos que ", { continued: true, align: "justify" })
-      .font("Helvetica-Bold")
-      .text(body["pessoa-certificada"], { continued: true, align: "justify" })
-      .font("Helvetica")
-      .text(body["tipo-certificado"], { continued: true, align: "justify" })
-      .font("Helvetica-Bold")
-      .text(body["nome-curso"], { continued: true, align: "justify" })
-      .font("Helvetica")
-      .text(".", { align: "justify" });
+    .fontSize(18)
+    .fill("#021c27")
+    .font("Helvetica")
+    .text("Certificamos que ", { continued: true, align: "justify" })
+    .font("Helvetica-Bold")
+    .text(body["pessoa-certificada"], { continued: true, align: "justify" })
+    .font("Helvetica")
+    .text(body["tipo-certificado"], { continued: true, align: "justify" })
+    .font("Helvetica-Bold")
+    .text(body["nome-curso"], { continued: true, align: "justify" })
+    .font("Helvetica")
+    .text(".", { align: "justify" });
 
     doc
-      .fontSize(12)
-      .moveDown(0.2)
-      .fill("#021c27")
-      .font("Helvetica")
-      .text("Ministrado por ", {
-        continued: true,
-        align: "justify",
-        indent: 28
-      });
+    .fontSize(12)
+    .moveDown(0.2)
+    .fill("#021c27")
+    .font("Helvetica")
+    .text("Ministrado por ", {
+      continued: true,
+      align: "justify",
+      indent: 28
+    });
 
-    function add_ministrantes(doc, ministrantes) {
-      let i = 0;
-      const limit = ministrantes.length - 1;
-      for (m of ministrantes) {
+    const add_ministrantes = (doc, ministrantes) => {
+      ministrantes.forEach((element, index) => {
         doc
-          .font("Helvetica-Bold")
-          .text(m, { continued: true, align: "justify" });
-        if (limit != i++) {
-          if (limit == i) {
-            doc
-              .font("Helvetica")
-              .text(" e ", { continued: true, align: "justify" });
-          } else {
-            doc
-              .font("Helvetica")
-              .text(", ", { continued: true, align: "justify" });
-          }
-        }
-      }
+        .font("Helvetica-Bold")
+        .text(element, { continued: true, align: "justify" });
+
+        if((ministrantes.length - index) == 2) doc.font("Helvetica").text(" e ", { continued: true, align: "justify" });
+        else if((ministrantes.length - index) > 2) doc.font("Helvetica").text(", ", { continued: true, align: "justify" });
+      })
+
       doc.font("Helvetica").text(".", { align: "justify" });
     }
-    add_ministrantes(doc, body["responsaveis-atividade"]);
 
-    //jumpLine(doc, 11);
+    add_ministrantes(doc, body["responsaveis-atividade"]);
 
     let oldBottomMargin = doc.page.margins.bottom;
     doc.page.margins.bottom = 0;
     doc
-      .fontSize(12)
-      .font("Helvetica")
-      .text(
-        "Carga horária: 30 horas",
-        doc.x,
-        doc.page.height - oldBottomMargin,
-        {
-          align: "center"
-        }
-      )
-      .moveDown(0.5)
-      .fill("#021c27")
-      .font("Times-BoldItalic")
-      .text(body["cidade-e-data"], {
-        align: "center"
-      });
+    .fontSize(12)
+    .font("Helvetica")
+    .text(
+      "Carga horária: 30 horas",
+      doc.x,
+      doc.page.height - oldBottomMargin, { align: "center" }
+    )
+    .moveDown(0.5)
+    .fill("#021c27")
+    .font("Times-BoldItalic")
+    .text(body["cidade-e-data"], {
+      align: "center"
+    });
+
 
     // Signatures
     doc.lineWidth(1);
@@ -197,98 +177,29 @@ export default async (req) => {
     doc.fillAndStroke("#021c27");
     doc.strokeOpacity(0.2);
 
-    //const startLine1 = 128;
-    const endLine1 = 128 + lineSize;
-    // doc
-    //   .moveTo(startLine1, signatureHeight)
-    //   .lineTo(endLine1, signatureHeight)
-    //   .stroke();
 
+    const endLine1 = 128 + lineSize;
     const startLine2 = endLine1 + 32;
     const endLine2 = startLine2 + lineSize;
+    
     doc
-      .moveTo(startLine2, signatureHeight)
-      .lineTo(endLine2, signatureHeight)
-      .stroke();
-
-    // const startLine3 = endLine2 + 32;
-    // const endLine3 = startLine3 + lineSize;
-    // doc
-    //   .moveTo(startLine3, signatureHeight)
-    //   .lineTo(endLine3, signatureHeight)
-    //   .stroke();
-
-    // doc
-    //   .font('Helvetica')
-    //   .fontSize(10)
-    //   .fill('#021c27')
-    //   .text('John Doe', startLine1, signatureHeight + 10, {
-    //     columns: 1,
-    //     columnGap: 0,
-    //     height: 40,
-    //     width: lineSize,
-    //     align: 'center',
-    //   });
-
-    // doc
-    //   .font('Helvetica')
-    //   .fontSize(10)
-    //   .fill('#021c27')
-    //   .text('', startLine1, signatureHeight + 25, {
-    //     columns: 1,
-    //     columnGap: 0,
-    //     height: 40,
-    //     width: lineSize,
-    //     align: 'center',
-    //   });
+    .moveTo(startLine2, signatureHeight)
+    .lineTo(endLine2, signatureHeight)
+    .stroke();
 
     doc
-      .font("Helvetica")
-      .fontSize(10)
-      .fill("#021c27")
-      .text(body["assinatura"], startLine2, signatureHeight + 10, {
-        columns: 1,
-        columnGap: 0,
-        height: 40,
-        width: lineSize,
-        align: "center"
-      });
+    .font("Helvetica")
+    .fontSize(10)
+    .fill("#021c27")
+    .text(body["assinatura"], startLine2, signatureHeight + 10, {
+      columns: 1,
+      columnGap: 0,
+      height: 40,
+      width: lineSize,
+      align: "center"
+    }
+    );
 
-    /*doc
-            .font('Helvetica')
-            .fontSize(10)
-            .fill('#021c27')
-            .text(body["cargo-assinatura"], startLine2, signatureHeight + 25, {
-                columns: 1,
-                columnGap: 0,
-                height: 40,
-                width: lineSize,
-                align: 'center',
-            });*/
-
-    // doc
-    //   .font('Helvetica')
-    //   .fontSize(10)
-    //   .fill('#021c27')
-    //   .text('Jane Doe', startLine3, signatureHeight + 10, {
-    //     columns: 1,
-    //     columnGap: 0,
-    //     height: 40,
-    //     width: lineSize,
-    //     align: 'center',
-    //   });
-
-    // doc
-    //   .font('Helvetica')
-    //   .fontSize(10)
-    //   .fill('#021c27')
-    //   .text('Director', startLine3, signatureHeight + 25, {
-    //     columns: 1,
-    //     columnGap: 0,
-    //     height: 40,
-    //     width: lineSize,
-    //     align: 'center',
-    //   });
 
     const tamanhoQR = 70;
 
@@ -324,11 +235,11 @@ export default async (req) => {
       });
 
       stream.on("end", () => {
-        res.status(200).send({ data: pdfB64 });
+        return { data: pdfB64 };
       });
     });
   } catch (err) {
-    res.status(500).send("Erro ao gerar o certificado");
+    console.log("Erro ao gerar o certificado");
     throw err;
   }
 };
