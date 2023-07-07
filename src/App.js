59d5c6e19cd282958ac75d60f46c1c6f3a4b6476
Enzo Weder <enzowederma@gmail.com>
2023-07-07 11:33:39 -0300

    🌈 style: add PET-SIMC logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './styles/App.css';
 import PETCert from './PETCert';
+import PETLogo from './assets/logo.jpeg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <main>
+        <div id="logo"><img src={PETLogo} alt="PET-SIMC Logo" srcset="" /></div>
         <PETCert />
       </main>
       <footer>© 2023 - Sistemas de Informação. Todos os direitos reservados.</footer>
