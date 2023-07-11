d94e6ea53ae6f0eced10dde28169157e358e97bc
Enzo Weder <enzowederma@gmail.com>
2023-07-11 11:20:54 -0300

    ✨ feat: add NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './styles/App.css';
 import PETCert from './PETCert';
 import PETLogo from './assets/logo.jpeg'
+import { generateRadio } from './Utils';
 
 function App() {
+  const navOptions = ["Gerar", "Verificar", "Historico"]
+
   return (
     <div className="App">
       <header className="App-header"></header>
       <main>
         <div id="logo"><img src={PETLogo} alt="PET-SIMC Logo" srcset="" /></div>
         <PETCert />
+        <nav><div>{navOptions.map((option) => generateRadio(option))}</div></nav>
       </main>
       <footer>© 2023 - Sistemas de Informação. Todos os direitos reservados.</footer>
     </div>
