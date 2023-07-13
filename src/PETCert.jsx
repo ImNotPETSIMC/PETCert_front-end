8225a062249beea688792b5a02c49f7aba4d7b55
Enzo Weder <enzowederma@gmail.com>
2023-07-13 14:17:30 -0300

    🎈 perf: change petcert inputs labels

--- src/PETCert.jsx
+++ src/PETCert.jsx
@@ -5,7 +5,7 @@ import { PDF64_API, BACKEND_API } from './getEnv';
 import './styles/PETCert.css';
 
 function PETCert() {   
-    const inputNames = ["Nome Curso", "Tipo Certificado", "Pessoa Certificada", "Responsaveis Atividade", "Cidade e Data", "Nome Assinante"];
+    const inputNames = [{name: "Nome do Curso", value: "nome_curso"}, {name:"Tipo de Certificado", value:"tipo_certificado"}, {name: "Pessoa Certificada", value:"pessoa_certificada"}, {name: "Responsáveis pela Atividade", value: "responsaveis_atividade"}, { name: "Cidade e Data", value: "cidade_e_data"}, { name: "Nome do Assinante", value: "nome_assinante"}];
     const [inputValues, setInputValues] = useState({
         pessoa_certificada: "",
         nome_curso: "",
@@ -24,7 +24,7 @@ function PETCert() {
         <div className="PETCert">
             <h1>PETCERT</h1>
             <div id='inputs-container'>
-                {inputNames.map( (input) => { return <Input name={input} fn={handleChange}/> }  )   }
+                {inputNames.map((input) => { return <Input value={input.value} name={input.name} fn={handleChange}/> }  )   }
                 <button type="submit" id='getCertificate' onClick={() => { getCertificate(PDF64_API, BACKEND_API, inputValues); }}>GERAR CERTIFICADO</button>
             </div>
         </div>
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,9 @@
 export const Input = (props) => {
-  const aux = String(props.name).replace(" ", "_");
-  const id = String(aux).toLowerCase();
 
   return (
-    <div className="input-container" key={id}>
-      <input type="text" name={id} id={id} placeholder={props.name} onChange={props.fn} />
-      <label htmlFor={id}>{props.name.toUpperCase()}</label>
+    <div className="input-container">
+      <input type="text" name={props.value} id={props.value} placeholder={props.name} onChange={props.fn} />
+      <label htmlFor={props.value}>{props.name.toUpperCase()}</label>
     </div>
   );
 };
